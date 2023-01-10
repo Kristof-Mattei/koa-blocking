9b3a19c7baba7754bb5e32a9344f8f470767f4b5
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2023-01-10 14:15:47 -0800

    fix: formatting II

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -1,15 +1,5 @@
-import { performance } from "perf_hooks";
-
-import { Foo } from "../service/foo";
-
-describe("a", () => {
-    it("foo is bar", () => {
-        expect(Foo()).toBe("Bar");
-    });
-});
-
-describe("b", () => {
-    it("external import works", () => {
-        expect(performance.now()).not.toBeNull();
+describe("test", () => {
+    it("works", () => {
+        expect(1 + 2).toBe(3);
     });
 });
